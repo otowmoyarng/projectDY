364af46815acfc8028b47f9f0ee123b50e6e7a6a
otowmoyarng <hidemusiam@gmail.com>
2021-09-26 10:56:50 +0900

    react-router-dom追加

---
+++ front-app/src/Menu.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="#">時刻表</a>
+                </li>
+                <li>
+                    <a href="#">運行予定</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;
