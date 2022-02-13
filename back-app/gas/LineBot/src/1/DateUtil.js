7dd7e62b2346cb20fd7fb76590ebc12a9e71d766
otowmoyarng <hidemusiam@gmail.com>
2022-02-13 12:04:43 +0900

    前月と翌月の処理を追加

--- back-app/gas/LineBot/src/1/DateUtil.js
+++ back-app/gas/LineBot/src/1/DateUtil.js
@@ -3,7 +3,7 @@ class DateUtil {
     /**
      * 現在日付をyyyy/MM/dd形式で取得する
      * @param date 日付
-     * @returns 
+     * @returns 日付文字列
      */
     static GetCurrentYmd(date) {
         return Utilities.formatDate((date ? date : new Date()), 'Asia/Tokyo', 'yyyy/MM/dd');
@@ -12,7 +12,7 @@ class DateUtil {
     /**
      * 現在日付をyyyy/MM形式で取得する
      * @param date 日付
-     * @returns 
+     * @returns 日付文字列
      */
     static GetCurrentYm(date) {
         return Utilities.formatDate((date ? date : new Date()), 'Asia/Tokyo', 'yyyy/MM');
@@ -21,6 +21,7 @@ class DateUtil {
     /**
      * 日付文字列からDateオブジェクトに変換する
      * @param dateStrings 日付文字列
+     * @returns Dateオブジェクト
      */
     static Convert(dateStrings) {
 
@@ -44,5 +45,33 @@ class DateUtil {
             dateSplit.push(1);
         }
         return new Date(dateSplit[0], dateSplit[1] - 1, dateSplit[2]);
+    }
+
+    /**
+     * 指定日時の前月を返す。
+     * @param date 日付
+     * @returns Dateオブジェクト
+     */
+    static PreviewMonth(date) {
+        if (date.getMonth() === 0) {
+            return new Date(date.getFullYear() - 1, 11, date.getDate());
+        } else {
+            date.setMonth(date.getMonth() - 1);
+            return date
+        }
+    }
+
+    /**
+     * 指定日時の翌月を返す。
+     * @param date 日付
+     * @returns Dateオブジェクト
+     */
+    static NextMonth(date) {
+        if (date.getMonth() === 11) {
+            return new Date(date.getFullYear() + 1, 0, date.getDate());
+        } else {
+            date.setMonth(date.getMonth() + 1);
+            return date
+        }
     }
 }
--- back-app/gas/LineBot/test/DateUtil.js
+++ back-app/gas/LineBot/test/DateUtil.js
@@ -9,3 +9,17 @@ function DateUtilTest_Convert() {
         }
     });
 }
+
+function DateUtilTest_PreviewMonth() {
+    const params = ["2021/12/10", "2022/01/01", "2022/02/22"];
+    params.forEach(param => {
+        console.log(`${param} -> ${DateUtil.GetCurrentYmd(DateUtil.PreviewMonth(DateUtil.Convert(param)))}`);
+    });
+}
+
+function DateUtilTest_NextMonth() {
+    const params = ["2021/11/15", "2021/12/10", "2022/01/31"];
+    params.forEach(param => {
+        console.log(`${param} -> ${DateUtil.GetCurrentYmd(DateUtil.NextMonth(DateUtil.Convert(param)))}`);
+    });
+}
